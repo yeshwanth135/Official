77d0d611e490d3cb033f8d66f3526ff393121ffe
Arun18297 <venkataarunkumar182@gmail.com>
2021-11-15 11:54:10 +0530

    Delete Me

---
+++ src/test/java/com/practice/ArunsDelete.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class ArunsDelete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Arun Edited");
+	}
+
+}
